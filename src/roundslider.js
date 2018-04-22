5baf59a321bcc8c7f369e19c5f057107a604005e
Soundara Pandian <soundar.kpr@gmail.com>
2018-04-22 13:44:20 +0530

    Updated the version to 1.3.2

--- src/roundslider.js
+++ src/roundslider.js
@@ -1,5 +1,5 @@
 /*!
- * roundSlider v1.3.1 | (c) 2015-2016, Soundar
+ * roundSlider v1.3.2 | (c) 2015-2018, Soundar
  * MIT license | http://roundsliderui.com/licence.html
  */
 
@@ -17,7 +17,7 @@
     RoundSlider.prototype = {
 
         pluginName: pluginName,
-        version: "1.3.1",
+        version: "1.3.2",
 
         // after the control initialization the updated default values
         // are merged into the options
